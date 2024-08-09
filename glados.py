c6c563f16e60f6df9bba3ca9fa92655a983d5800
CYF <1933469479@qq.com>
2024-08-10 01:08:10 +0800

    1

--- glados.py
+++ glados.py
@@ -9,57 +9,55 @@ if __name__ == "__main__":
     # 推送内容
     sendContent = ""
     # glados账号cookie 直接使用数组 如果使用环境变量需要字符串分割一下
-    cookies = os.environ.get("GLADOS_COOKIE", []).split(";")
-    if cookies[0] == "":
+    cookie = os.environ.get("GLADOS_COOKIE", []).split(";")
+    if cookie == "":
         print("未获取到COOKIE变量")
-        cookies = []
-        exit(0)
+        exit(1)
     url = "https://glados.rocks/api/user/checkin"
     url2 = "https://glados.rocks/api/user/status"
     referer = "https://glados.rocks/console/checkin"
     origin = "https://glados.rocks"
     useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36"
     payload = {"token": "glados.one"}
-    for cookie in cookies:
-        checkin = requests.post(
-            url,
-            headers={
-                "cookie": cookie,
-                "referer": referer,
-                "origin": origin,
-                "user-agent": useragent,
-                "content-type": "application/json;charset=UTF-8",
-            },
-            data=json.dumps(payload),
+    checkin = requests.post(
+        url,
+        headers={
+            "cookie": cookie,
+            "referer": referer,
+            "origin": origin,
+            "user-agent": useragent,
+            "content-type": "application/json;charset=UTF-8",
+        },
+        data=json.dumps(payload),
+    )
+    state = requests.get(
+        url2,
+        headers={
+            "cookie": cookie,
+            "referer": referer,
+            "origin": origin,
+            "user-agent": useragent,
+        },
+    )
+    # --------------------------------------------------------------------------------------------------------#
+    time = state.json()["data"]["leftDays"]
+    time = time.split(".")[0]
+    email = state.json()["data"]["email"]
+    if "message" in checkin.text:
+        mess = checkin.json()["message"]
+        print(
+            email + "----结果--" + mess + "----剩余(" + time + ")天"
+        )  # 日志输出
+        sendContent += email + "----" + mess + "----剩余(" + time + ")天\n"
+    else:
+        requests.get(
+            "http://www.pushplus.plus/send?token="
+            + sckey
+            + "&content="
+            + email
+            + "cookie已失效"
         )
-        state = requests.get(
-            url2,
-            headers={
-                "cookie": cookie,
-                "referer": referer,
-                "origin": origin,
-                "user-agent": useragent,
-            },
-        )
-        # --------------------------------------------------------------------------------------------------------#
-        time = state.json()["data"]["leftDays"]
-        time = time.split(".")[0]
-        email = state.json()["data"]["email"]
-        if "message" in checkin.text:
-            mess = checkin.json()["message"]
-            print(
-                email + "----结果--" + mess + "----剩余(" + time + ")天"
-            )  # 日志输出
-            sendContent += email + "----" + mess + "----剩余(" + time + ")天\n"
-        else:
-            requests.get(
-                "http://www.pushplus.plus/send?token="
-                + sckey
-                + "&content="
-                + email
-                + "cookie已失效"
-            )
-            print("cookie已失效")  # 日志输出
+        print("cookie已失效")  # 日志输出
     # --------------------------------------------------------------------------------------------------------#
     if sckey != "":
         requests.get(
